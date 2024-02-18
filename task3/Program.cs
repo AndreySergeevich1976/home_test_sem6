aaa034acf8e117ef7b1c58e1a4fcc4507d3571c2
Andrey Krasnov <tiralmaz@mail.ru>
2024-02-18 19:43:47 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,21 @@
+﻿bool Answer (string str){
+    for (int i=0;i<str.Length/2;i++){
+        if (str[i]!=str[str.Length-1-i]){
+            return false;
+        }
+
+    }
+    return true;
+}
+void PrintAnswer(bool answer){
+    if (answer==true){
+    Console.WriteLine("Yes");
+    }
+    else {
+    Console.WriteLine("No");
+    }
+}
+Console.Clear();
+Console.Write("Введите строку: ");
+string str = Console.ReadLine()!;
+PrintAnswer(Answer(str));
